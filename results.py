4372476f23a0d002a927b23aca697551f33a7b89
alana170 <52054877+alana170@users.noreply.github.com>
2020-06-12 16:49:39 -0400

    edited pages

--- results.py
+++ results.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('forms.html')
+
+@app.route('/', method = ['POST'])
+def getValues():
+    name = request.form['fname']
+    age = request.form['lname']
+    db = request.form['q1']
+    return render_template('action_page.html', n = name, age = age, db =db)
+
+
+
+if __name__=='__main__' :
+    app.run(debug=True)
+
 
