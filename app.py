1a221c16eb466eb52bcf39166b5712698ae36502
alana170 <52054877+alana170@users.noreply.github.com>
2020-07-01 17:49:30 -0400

    changed Database to book3 uploaded books and designed table

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,  render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import csv
 
@@ -6,37 +6,49 @@ import csv
 #from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book3.db'
 db = SQLAlchemy(app)
 
 class Book(db.Model):
     BookID = db.Column(db.Integer, primary_key = True)
-    Isbn = db.Column(db.String(100), unique = True)
-    Title = db.Column(db.String(255), unique=True, nullable=False)
+    Isbn = db.Column(db.String(100), unique = False)
+    Title = db.Column(db.String(255), unique=False, nullable=False)
     Author = db.Column(db.String(255), unique=False, nullable=True)
     Year = db.Column(db.Integer, nullable= True) 
 
     def __repr__(self):
         return 'Book ID = ' + str(self.BookID)
 
-@app.route('/')
+
+@app.route('/books')
 def index(): 
     return render_template('books.html', books = Book.query.all())
 
-@app.route('/uploadFile/')   
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/uploadFile')   
 def uploadFile():
     f = open("C:/Users/sgoru/project1/project1/books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
-        if(reader.line_num>1) : 
-            new_book = Book(Isbn = isbn, Title = title, Author = author, Year = year)
-            db.session.add(new_book) 
-            db.session.commit()
+        if reader.line_num>1 : 
+            if db.session.query(Book).filter_by(Isbn = isbn) == None:
+                new_book = Book(Isbn = isbn, Title = title, Author = author, Year = year)
+                db.session.add(new_book) 
+                db.session.commit()
     return 'File Uploaded'
     
-@app.route('/book/<string: isbn>')
-def ratingInfoByISBN():
-    return 'Rating Info Goes Here'
+# @app.route('/book/<string: isbn>')
+# def ratingInfoByISBN():
+#     return 'Rating Info Goes Here'
 
 
 
